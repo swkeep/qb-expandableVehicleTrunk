938886c137d3fdae68e49e2c2c51849fc64e503d
swkeep <badaosoft@gmail.com>
2022-01-06 11:41:51 +0330

    add sv_functions.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ client_scripts {'@PolyZone/client.lua', 'client/functions.lua', 'client/cl_main.
 
 shared_script {'config.lua', 'shared/shared.lua', "@qb-core/shared.lua"}
 
-server_script {'server/sv_main.lua'}
+server_script {'server/sv_main.lua','server/sv_functions.lua'}
 
 files {'html/*', 'html/css/*', 'html/css/font/*', 'html/js/*', 'html/img/*'}
 
--- server/sv_main.lua
+++ server/sv_main.lua
@@ -87,7 +87,12 @@ function calculateUpgradeAmount(src, upgradeReqData, weightUpgrades)
                     canUpgrade = (currentCarryWeight + (total * step) <= vehicleMeta.maxWeight) and
                                      (currentCarryWeight + (total * step) ~= currentCarryWeight)
                     if canUpgrade == true then
-                        removeMoney(src , 'cash' , vehicleMeta.stepPrice * total , desc)
+                        local paid = removeMoney(src, 'cash', vehicleMeta.stepPrice * total, 'trunk')
+                        if paid then
+                            canUpgrade = true
+                        else
+                            canUpgrade = false
+                        end
                     end
                     return canUpgrade, (currentCarryWeight + (total * step))
                 end
@@ -104,114 +109,6 @@ AddEventHandler('keep-carInventoryWeight:server:OpenUI', function()
 end)
 
 -- ============================
---      Functions
--- ============================
-function removeMoney(src, type, amount, desc)
-    local plyert = QBCore.Functions.GetPlayer(src)
-    -- local plyCid = ply.PlayerData.citizenid
-    if plyert.Functions.RemoveMoney(type, amount, "vehicle-upgrade-bail-" .. desc) then
-        return true
-    end
-    return false
-end
-
-function createServerResponse(Result, class)
-    -- create server response when client fetch data
-    local weightUpgrades = oxmysql:scalarSync('SELECT weightUpgrades from player_vehicles where plate = ?',
-        {Result[1]["plate"]})
-    local characterINFO = json.decode(Result[1]['charinfo'])
-    local sv_response = {}
-    Upgrades = {}
-
-    sv_response['vehicleInfo'] = {
-        vehicle = Result[1]["vehicle"],
-        plate = Result[1]["plate"],
-        maxweight = Result[1]["maxweight"],
-        hash = Result[1]["hash"],
-        class = class
-    }
-    sv_response['characterInfo'] = {
-        firstname = characterINFO["firstname"],
-        lastname = characterINFO["lastname"],
-        cid = characterINFO["cid"],
-        phone = characterINFO["phone"],
-        gender = characterINFO["gender"]
-    }
-
-    -- calculate upgrade steps 
-    for Type, Vehicle in pairs(Config.Vehicles) do
-        if Type == class then
-            for name, vehicleMeta in pairs(Vehicle) do
-                if name == Result[1]["vehicle"] then
-                    Upgrades = createUpgrades(vehicleMeta, weightUpgrades, Result[1])
-                end
-            end
-        end
-    end
-    sv_response['upgrades'] = Upgrades
-    return sv_response
-end
-
-function createUpgrades(vehicleMeta, weightUpgrades, vehicle)
-    local temp = {}
-    local weightUpgrades = json.decode(weightUpgrades)
-    local step = (vehicleMeta.maxWeight - vehicleMeta.minWeight) / vehicleMeta.upgrades
-
-    if weightUpgrades ~= nil then
-        for k, value in pairs(weightUpgrades) do
-            temp[k] = value
-        end
-        temp["step"] = step
-        temp["stepPrice"] = vehicleMeta.stepPrice
-        return temp
-    else
-        return initWeightUpgradesData(vehicleMeta, vehicle, step)
-    end
-end
-
-function initWeightUpgradesData(vehicleMeta, vehicle, step)
-    -- init vehicle
-    local initWeightUpgrades = {}
-    for i = 1, vehicleMeta.upgrades, 1 do
-        table.insert(initWeightUpgrades, string.format('"%s":%s', i, false))
-    end
-
-    updateVehicleDatabaseValues(vehicleMeta.minWeight, initWeightUpgrades, vehicle)
-
-    -- step , stepPrice is for client to show to players
-    table.insert(initWeightUpgrades, string.format('"%s":%s', 'step', step))
-    table.insert(initWeightUpgrades, string.format('"%s":%s', 'stepPrice', vehicleMeta.stepPrice))
-
-    initWeightUpgrades = "{" .. table.concat(initWeightUpgrades, ",") .. "}"
-    return json.decode(initWeightUpgrades)
-end
-
-function updateVehicleDatabaseValues(maxweight, weightUpgradesChanges, upgradeReqData)
-    local plate = upgradeReqData["plate"]
-    local model = upgradeReqData["vehicle"] or upgradeReqData["model"]
-
-    local hash = upgradeReqData["hash"]
-    local weightUpgradesChanges2 = "{" .. table.concat(weightUpgradesChanges, ",") .. "}"
-
-    oxmysql:update('UPDATE `player_vehicles` SET maxweight = ? WHERE vehicle = ? AND hash = ? AND plate = ?',
-        {maxweight, model, hash, plate}, function(result)
-            print(result, "maxweight updated")
-        end)
-    oxmysql:update('UPDATE `player_vehicles` SET weightUpgrades = ? WHERE vehicle = ? AND hash = ? AND plate = ?',
-        {weightUpgradesChanges2, model, hash, plate}, function(result)
-            print(result, "weightUpgrades updated")
-        end)
-end
-
-function sortTable(table)
-    local temp = {}
-    for k, value in pairs(table) do
-        temp[k] = value
-    end
-    return temp
-end
-
--- ============================
 --      Commands
 -- ============================
 
@@ -219,20 +116,3 @@ CoreName.Commands.Add("testOpen", "Spawn Animals (Admin Only)", {{"model", "Anim
     function(source, args)
         TriggerClientEvent('keep-carInventoryWeight:Client:OpenUI', source, args[1])
     end, 'admin')
-
-function tprint(tbl, indent)
-    if not indent then
-        indent = 0
-    end
-    for k, v in pairs(tbl) do
-        formatting = string.rep("  ", indent) .. k .. ": "
-        if type(v) == "table" then
-            print(formatting)
-            tprint(v, indent + 1)
-        elseif type(v) == 'boolean' then
-            print(formatting .. tostring(v))
-        else
-            print(formatting .. v)
-        end
-    end
-end
