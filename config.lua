b2d9e3bc25b3ff45819dc66e2c2abb10588f640b
swkeep <badaosoft@gmail.com>
2022-01-08 14:18:14 +0330

    add blowtorch timer to config file

--- config.lua
+++ config.lua
@@ -5,27 +5,29 @@ Config.DEBUG = true -- make sure it's false
 --       Server Config
 -- ============================
 
+Config.blowtorchTime = math.random(1000,15000);
+
 Config.Vehicles = {
     [7] = { -- table keys are vehicles classes https://docs.fivem.net/natives/?_0x29439776AAA00A62
         ["sultanrsv8"] = {
-            minWeight = 10000,
-            maxWeight = 100000,
+            minCarryCapacity = 10000,
+            maxCarryCapacity = 100000,
             upgrades = 4,
             stepPrice = 1000
         }
     },
     [9] = {
         ["dubsta3"] = {
-            minWeight = 30000,
-            maxWeight = 150000,
+            minCarryCapacity = 30000,
+            maxCarryCapacity = 150000,
             upgrades = 2,
             stepPrice = 5000
         }
     },
     [12] = {
         ["rumpo3"] = {
-            minWeight = 50000,
-            maxWeight = 200000,
+            minCarryCapacity = 50000,
+            maxCarryCapacity = 200000,
             upgrades = 5,
             stepPrice = 800
         }
